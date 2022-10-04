b852673cc473da34032456ac2776129ad81fdd6a
karlaujo <karlaujo168@gmail.com>
2022-10-04 13:00:49 -0400

    update

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -1,102 +1,140 @@
-//1.1 LECTURE: Values and Variables
-const country = "Canada";
-const continent = "North America";
-let population = 38526760;
+/* Coding challenge #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height invariables
+2. Calculate both their BMI susing the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+GOOD LUCK 😀
+*/ 
 
-console.log(country + "," + continent + "," + population);
-
-//1.2 LECTURE: Data Types 
-const isIsland = false;
-let language;
-
-console.log(isIsland + ", " + country + ", " + continent + ", " + population + ", " + language);
-
-//1.3 LECTURE: let, const, var
-language = "english";
-//country = "france";
-
-//1.4 LECTURE: Basic Operators
-let halfPopulation = population / 2;
-console.log(halfPopulation);
-
-population++;
-console.log(population);
-population--;
-
-let finlandPopulation = 6000000;
-let averagePopulation = 33000000;
-let avgResult = population - averagePopulation;
+//1.1 Data 1
+const markMass=78;
+const markHeight=1.69;
+const johnMass=92;
+const johnHeight=1.95;
 
 
+//2. BMI calculations
+const johnBMI = johnMass / johnHeight ** 2;
+const markBMI = markMass / markHeight ** 2;
+const markHigherBMI = markBMI > johnBMI;
 
-let description = "Canada is in North America, and its 38626760 people speak english and french";
-console.log(description);
+console.log(johnBMI, markBMI, markHigherBMI);
 
-//1.5 LECTURE: Strings and Template literals
-description = `${country} is in ${continent}, and its ${population} people speak ${language}`;
-console.log(description);
+//4. Test Data 2
+const markMass2 = 95;
+const markHeight2 = 1.88;
+const johnMass2 = 85;
+const johnHeight2 = 1.76;
 
-//1.6 LECTURE: Taking Decisions: if / else Statements
-if (averagePopulation > population) {
-    console.log(`${country} has a smaller population than the average country`);
+const markBMI2 = markMass2 / markHeight2 ** 2;
+const johnBMI2 = johnMass2 / johnHeight2 ** 2;
+const markHigherBMI2 = markBMI2 > johnBMI2;
+
+//5.
+console.log( markBMI2, johnBMI2, markHigherBMI2);
+
+
+//Strings and templates 
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 2001;
+const year = 2022;
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + 
+' year old ' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`
+console.log(jonasNew);
+
+
+console.log(`Just a regular string...`);
+
+console.log(`String with 
+multiple
+lines`);
+
+///////////////////////////////////////
+///////////////////////////////////////
+
+/* Coding Challenge #2
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+20%.
+Your tasks:
+1. Write a function 'calcTip' that takes any bill value as an input and returns
+the corresponding tip, calculated based on the rules above (you can check out
+the code from first tip calculator challenge if you need to). Use the function
+type you like the most. Test the function using a bill value of 100
+2. And now let's use arrays! So create an array 'bills' containing the test data
+below
+3. Create an array 'tips' containing the tip value for each bill, calculated from
+the function you created before
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+Test data: 125, 555 and 44 */
+
+const bills = [];
+let y;
+for (let y = 0; y < 16; y++) {
+    bills[y] = y * 30;
+}
+
+function calcTip(bills) {
+    let x;
+    let tip;
+
+  for (x=0; x < bills.length; x++) {
+    if (bills[x] >= 50 && bills[x] <= 300) {
+        tip = 0.15;
+      } else {
+          tip = 0.20;
+      }
+
+      console.log(`Bill: $${bills[x]} --- Tip: $${tip}\n`);
+  }
+  
+}
+calcTip(bills);
+
+
+/*Coding Challenge #3
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+implement the calculations! Remember: BMI = mass / height ** 2 = mass
+/ (height * height) (mass in kg and height in meter)
+Your tasks:
+1. For each of them, create an object with properties for their full name, mass, and
+height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+method on both objects). Store the BMI value to a property, and also return it
+from the method
+3. Log to the console who has the higher BMI, together with the full name and the
+respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+tall. */
+
+const mark = {
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+    calcBMI: 78 / 1.69 ** 2
+};
+
+const john = {
+    fullName: "John Smith",
+    mass: 92,
+    height: 1.95,
+    calcBMI: 92 / 1.95 ** 2
+}
+
+if (mark.calcBMI < john.calcBMI) {
+    console.log(`${john.fullName}'s BMI (${john.calcBMI}) is higher than ${mark.fullName}'s BMI (${mark.calcBMI})!`);
 } else {
-    console.log(`${country} has a larger population than the average country`);
+    console.log(`${mark.fullName}'s BMI (${mark.calcBMI}) is lower than ${mark.fullName}'s BMI (${mark.calcBMI})`);
 }
-
-//1.7 LECTURE: Type Conversion and Coercion
-console.log('9'-'5');
-console.log('19'-'13'+'17');
-console.log('19'-'13' + 17);
-console.log('123' < 57);
-console.log(5 + 6 + '4' + 9 - 4 - 2);
-
-//1.8 LECTURE: Equality Operators: == vs. ===
-/*
-let numNeighbours = prompt('How many neighbour countries does your country have?');
-if (numNeighbours === 1)
-    console.log('Only 1 border!');
-else if (numNeighbours > 1)
-    console.log(numNeighbours+' borders!');
-else {
-    console.log('No border!');
-}
-*/
-
-//1.9 LECTURE: Logical Operators
-
-if (population < 50000000 && !isIsland) 
-    console.log(`${country} is the right country for you! :)`);
-else
-    console.log(`${country} is the wrong country for you :(`);
-
-//2.0 LECTURE: The switch Statement
-switch (language) {
-    case 'chinese' || 'mandarin': 
-        console.log('MOST number of native speakers');
-        break;
-
-    case 'spanish': 
-        console.log('2nd place in number of native speakers');
-        break;
-
-    case 'english': 
-        console.log('3rd place in number of native speakers');
-        break;
-
-    case 'hindi': 
-        console.log('4th place in number of native speakers');
-        break;
-
-    case 'arabic': 
-        console.log('5th place in number of native speakers');
-        break;
-
-    default : 
-        console.log('Great language as well :D');
-}
-
-//2.1 LECTURE:  The Condition (Ternary) Operator  
-if (population > 33000000) 
-    console.log(`${country}'s population is above average`);
-else
-    console.log(`${country}'s population is below average`);
--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,19 +1,112 @@
-// let js = 'amazing';
-// console.log(40+8+23-10);
+//1.1 LECTURE: Values and Variables
+const country = "Canada";
+const continent = "North America";
+let population = 38526760;
 
-// console.log("Jonas");
-// console.log(23);
+console.log(country + "," + continent + "," + population);
 
-// let firstName = "Karl";
+//1.2 LECTURE: Data Types 
+const isIsland = false;
+let language;
 
-// let myFirstJob = 'Programmer';
-// let myCurretnJob = 'Teacher';
+console.log(isIsland + ", " + country + ", " + continent + ", " + population + ", " + language);
 
-// let job1 = 'programmer'
-// let job2 = "teacher";
+//1.3 LECTURE: let, const, var
+language = "english";
+//country = "france";
 
-// console.log(myFirstJob);
+//1.4 LECTURE: Basic Operators
+let halfPopulation = population / 2;
+console.log(halfPopulation);
 
-// console.log(firstName);
-// console.log(firstName);
-// console.log(firstName);
+population++;
+console.log(population);
+population--;
+
+let finlandPopulation = 6000000;
+let averagePopulation = 33000000;
+let avgResult = population - averagePopulation;
+
+
+
+let description = "Canada is in North America, and its 38626760 people speak english and french";
+console.log(description);
+
+//1.5 LECTURE: Strings and Template literals
+description = `${country} is in ${continent}, and its ${population} people speak ${language}`;
+console.log(description);
+
+//1.6 LECTURE: Taking Decisions: if / else Statements
+if (averagePopulation > population) {
+    console.log(`${country} has a smaller population than the average country`);
+} else {
+    console.log(`${country} has a larger population than the average country`);
+}
+
+//1.7 LECTURE: Type Conversion and Coercion
+console.log('9'-'5');
+console.log('19'-'13'+'17');
+console.log('19'-'13' + 17);
+console.log('123' < 57);
+console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+//1.8 LECTURE: Equality Operators: == vs. ===
+/*
+let numNeighbours = prompt('How many neighbour countries does your country have?');
+if (numNeighbours === 1)
+    console.log('Only 1 border!');
+else if (numNeighbours > 1)
+    console.log(numNeighbours+' borders!');
+else {
+    console.log('No border!');
+}
+*/
+
+//1.9 LECTURE: Logical Operators
+
+if (population < 50000000 && !isIsland) 
+    console.log(`${country} is the right country for you! :)`);
+else
+    console.log(`${country} is the wrong country for you :(`);
+
+//2.0 LECTURE: The switch Statement
+switch (language) {
+    case 'chinese' || 'mandarin': 
+        console.log('MOST number of native speakers');
+        break;
+
+    case 'spanish': 
+        console.log('2nd place in number of native speakers');
+        break;
+
+    case 'english': 
+        console.log('3rd place in number of native speakers');
+        break;
+
+    case 'hindi': 
+        console.log('4th place in number of native speakers');
+        break;
+
+    case 'arabic': 
+        console.log('5th place in number of native speakers');
+        break;
+
+    default : 
+        console.log('Great language as well :D');
+}
+
+//2.1 LECTURE:  The Condition (Ternary) Operator  
+if (population > 33000000) 
+    console.log(`${country}'s population is above average`);
+else
+    console.log(`${country}'s population is below average`);
+
+    //if else statements
+const age = 15;
+const isOldEnough = age >= 16;
+const yearsLeft = 16 - age;
+
+if (isOldEnough) {
+    console.log(`Sarah can start driving 🚗`);
+} else {console.log(`Sarah is too young, she needs to wait another ${yearsLeft} year(s).`)
+}
